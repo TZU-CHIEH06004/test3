275b5408d7554ba23939e51068a938e04692b2bd
tzu <f060419@gmail.com>
2023-04-20 09:21:05 +0800

    1st

---
+++ PYA101.py
@@ -0,0 +1,13 @@
+#TODO
+n1 = eval(input())  #加eval才會變數值
+n2 = eval(input())
+n3 = eval(input())
+n4 = eval(input())
+#向右靠齊
+#TODO
+print('|{:>5d} {:>5d}|'.format(n1,n2))
+print('|{:>5d} {:>5d}|'.format(n3,n4))
+#向左靠齊
+#TODO
+print('|{:<5d} {:<5d}|'.format(n1,n2))
+print('|{:<5d} {:<5d}|'.format(n3,n4))
